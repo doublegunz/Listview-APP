56ccf486a3be965d7e8692daacc925dbc4436d45
Gun Gun Priatna <gungunpriatna02@gmail.com>
2019-04-30 06:09:57 +0700

    init project

---
+++ app/src/main/java/com/gungunpriatna/listviewapp/HeroAdapter.java
@@ -0,0 +1,78 @@
+package com.gungunpriatna.listviewapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Hero> heroes;
+
+    void setHeroes(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    HeroAdapter(Context context) {
+        this.context = context;
+        heroes = new ArrayList<>();
+    }
+
+
+    @Override
+    public int getCount() {
+        return heroes.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return heroes.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            //menghubungkan ke viewholder dengan view
+            view = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false);
+        }
+
+        //deklarasi viewholder
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        //ubah nilai hero sesuai posisi
+        Hero hero = (Hero) getItem(i);
+        viewHolder.bind(hero);
+        return view;
+    }
+
+
+    private class ViewHolder {
+        private TextView txtName;
+        private TextView txtDescription;
+        private ImageView imgPhoto;
+
+        ViewHolder(View view) {
+            //deklarasi objek dari layout
+            txtName = view.findViewById(R.id.txt_name);
+            txtDescription = view.findViewById(R.id.txt_description);
+            imgPhoto = view.findViewById(R.id.img_photo);
+        }
+
+        void bind(Hero hero) {
+            txtName.setText(hero.getName());
+            txtDescription.setText(hero.getDescription());
+            imgPhoto.setImageResource(hero.getPhoto());
+        }
+    }
+}
---
+++ app/src/main/java/com/gungunpriatna/listviewapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.gungunpriatna.listviewapp;
+
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    //deklarasi variable
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new HeroAdapter(this);
+
+        ListView listView = findViewById(R.id.lv_list);
+        listView.setAdapter(adapter);
+
+        prepare();
+
+        addItem();
+    }
+
+    private void prepare() {
+        //ambil data dari strings.xml
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+
+        //set adapter
+        adapter.setHeroes(heroes);
+
+    }
+}
